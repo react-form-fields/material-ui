e8af36ba55ead3e756e784d10fce8d34552cbe77
Daniel Prado <danielprado.ad@gmail.com>
2018-06-10 16:29:26 -0300

    update docs

--- docs/build.sh
+++ docs/build.sh
@@ -2,9 +2,15 @@ set -e
 
 BASEDIR=$(cd `dirname $0` && pwd)
 
+rm -rf $BASEDIR/static
+rm -rf $BASEDIR/asset-manifest.json
+rm -rf $BASEDIR/favicon.ico
+rm -rf $BASEDIR/index.html
+rm -rf $BASEDIR/manifest.json
+rm -rf $BASEDIR/service-worker.js
+
 (cd $BASEDIR/project && yarn build)
 
-rm -rf $BASEDIR/build
 rsync -avzP $BASEDIR/project/build/ $BASEDIR/
 
 rm -rf $BASEDIR/project/build
