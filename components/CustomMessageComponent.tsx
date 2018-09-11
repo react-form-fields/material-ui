9bc4bf346eae773f851155b60319b52e32599fd6
Daniel Prado <danielprado.ad@gmail.com>
2018-09-11 09:41:07 -0300

    add better support for es6 import

---
+++ components/CustomMessageComponent.tsx
@@ -0,0 +1,3 @@
+import CustomMessageComponent from '@react-form-fields/core/components/CustomMessage';
+
+export default CustomMessageComponent;
---
+++ components/ValidationContext.tsx
@@ -0,0 +1,3 @@
+import ValidationContextComponent from '@react-form-fields/core/components/ValidationContext';
+
+export default ValidationContextComponent;
