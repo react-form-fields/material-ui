dd12b108351fe4f71853701495139c6356c83c5b
danieloprado <danielprado.ad@gmail.com>
2019-04-23 15:19:57 -0300

    v1.19.2

---
+++ lang/en-us.ts
@@ -0,0 +1,22 @@
+import { IConfig } from '@react-form-fields/core/config';
+import coreLang from '@react-form-fields/core/lang/en-us';
+import * as locale from 'date-fns/locale/en-US';
+
+const langPTBR: IConfig = {
+  ...coreLang,
+  dateLocale: locale,
+  dateFormat: 'MM/dd/yyyy',
+  timeFormat: 'hh:mm a',
+  dateTimeFormat: 'MM/dd/yyyy hh:mm a',
+  dateLabels: {
+    cancel: 'Cancel',
+    ok: 'Ok',
+    clear: 'Clear'
+  },
+  trumbowyg: {
+    loadLocale: () => Promise.resolve(),
+    config: {}
+  }
+};
+
+export default langPTBR;
